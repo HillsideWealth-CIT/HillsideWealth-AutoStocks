9fb5ede7e3c8fe81554622aad08e9b0924600ed9
DominicTr <35462804+DominicTr@users.noreply.github.com>
2019-03-01 02:05:52 -0800

    Added update function (Don't click update)

--- actions/api_calls.js
+++ actions/api_calls.js
@@ -21,6 +21,7 @@ function summary_call(item) {
     })
 }
 
+
 function gurufocus_add(list) {
     var promises = [];
     for (let i = 0; i < list.length; i++) {
@@ -37,7 +38,91 @@ function gurufocus_add(list) {
     })
 }
 
+var symbols = ['AAPL', 'GOOGL', 'AMZN', 'TSX:ATZ', 'TSX:CP', 'TSX:DOL']
+
+const financials_call = (symbol, callback) => {
+
+	//Net debt = Long term debt + current portion of long term debt + minority interest - cash& cash equivalents - market securities
+    //aEBITDA = EBITDA + Stock Based Compensation
+    //ND aEBITDA = Net dept / aEBITDA
+    //aEBITDA % = aEBITDA / Revenue
+    //aEBITDA x AT = aEBITDA % * Asset Turnover
+    //EV/aEBITDA = Enterprise value / aEBITDA
+    //Spice = (aEBITA x AT * 100)/(EV/aEBITDA)
+    //ROE/Mult = ROE/EVaEBITDA
+
+	let data = {};
+        request({
+            url: `https://api.gurufocus.com/public/user/49791c4074556510da66a0296e8c672b:8850a7457029e3e220a7c15bf06be9ea/stock/${symbol}/summary`,
+            json: true
+        }, (error, response, body) => {
+            	if(error) {
+            		reject(error)
+            	}else{
+	            	data['Symbol'] = symbol;
+	            	data['Company'] = body.summary.general['company']
+	            	data['Price'] = parseFloat(body.summary.general['price'])
+            	}
+            });
+        request({
+        	url: `https://api.gurufocus.com/public/user/49791c4074556510da66a0296e8c672b:8850a7457029e3e220a7c15bf06be9ea/stock/${symbol}/financials`,
+        	json: true
+        }, (error, response, body) => {
+	        	if(error) {
+	        		reject(error)
+	        	}else{
+	        		var longTermDebt = parseFloat(body.financials.quarterly.balance_sheet['Long-Term Debt'].pop()),
+	        			currentLTB = parseFloat(body.financials.quarterly.balance_sheet['Current Portion of Long-Term Debt'].pop()),
+	        			minoriInt = parseFloat(body.financials.quarterly.balance_sheet['Minority Interest'].pop()),
+	        			cashACE = parseFloat(body.financials.quarterly.balance_sheet['Cash And Cash Equivalents'].pop()),
+	        			marketSec = parseFloat(body.financials.quarterly.balance_sheet['Marketable Securities'].pop());
+	        			netDebt = longTermDebt + currentLTB + minoriInt - cashACE - marketSec;
+	        		data["Net Debt"] = parseFloat(netDebt.toFixed(2));
+
+	        		var EBITDA = parseFloat(body.financials.quarterly.income_statement['EBITDA'].pop()),
+	        			stockComp = parseFloat(body.financials.quarterly.cashflow_statement['Stock Based Compensation'].pop()),
+	        			aEBITDA = EBITDA + stockComp;
+	        		data["aEBITDA"] = parseFloat(aEBITDA.toFixed(2));
+
+	        		var NDaEBITDA = netDebt/aEBITDA;
+	        		data["ND/aEBITDA"] = parseFloat(NDaEBITDA.toFixed(2));
+
+	        		var aEBITDAPer = aEBITDA / parseFloat(body.financials.quarterly.income_statement['Revenue'].pop()) * 100
+	        		data["Revenue"] = parseFloat(body.financials.quarterly.income_statement['Revenue'].pop())
+	        		data["aEBITDA %"] = parseFloat(aEBITDAPer.toFixed(2));
+
+	        		var aEBITDAxAT = aEBITDAPer * parseFloat(body.financials.quarterly.common_size_ratios['Asset Turnover'].pop())
+	        		data["aEBITDA x AT"] = parseFloat(aEBITDAxAT.toFixed(2));
+	        		data["Asset Turnover"] = parseFloat(body.financials.quarterly.common_size_ratios['Asset Turnover'].pop())
+
+	        		var EVaEBITDA = parseFloat(body.financials.quarterly.valuation_and_quality['Enterprise Value'].pop())/aEBITDA
+	        		data["EV/aEBITDA"] = parseFloat(EVaEBITDA.toFixed(2));
+
+	        		var spice = aEBITDAxAT/ EVaEBITDA;
+	        		data["Spice"] = parseFloat(spice.toFixed(2));
+
+	        		data['Yield'] = parseFloat(body.financials.quarterly.valuation_ratios['Dividend Yield %'].pop());
+	        		data['Market Cap'] = parseFloat(parseFloat(body.financials.quarterly.valuation_and_quality["Market Cap"].pop()).toFixed(2));
+	        		data['Enterprise Value'] = parseFloat(parseFloat(body.financials.quarterly.valuation_and_quality["Enterprise Value"].pop()).toFixed(2));
+	        		data['ROE'] = parseFloat(parseFloat(body.financials.quarterly.common_size_ratios["ROE %"].pop()).toFixed(2));
+
+	        		var ROEMult = parseFloat(body.financials.quarterly.common_size_ratios["ROE %"].pop())/EVaEBITDA;
+	        		data["ROE/Mult"] = parseFloat(ROEMult.toFixed(2));
+	        		console.log(data);
+	        		console.log();
+	        	}
+        });
+ };
+
+
+ function gurufocus_update(){
+ 	for(item in symbols) {
+ 		financials_call(symbols[item]);
+ 	}
+ }
+
 
 module.exports = {
-    gurufocus_add
+    gurufocus_add,
+    gurufocus_update
 }
