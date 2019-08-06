2ce08325e4ea4d5bb985ec3a530f6e34e973e0f8
Siming Yang <yangsiming1999@gmail.com>
2019-08-06 10:38:48 -0700

    changed variable for shared datatable columns

--- public/js/collection/sharedDataTable.js
+++ public/js/collection/sharedDataTable.js
@@ -177,8 +177,8 @@ function column_builder(){
         { data : "growth_rate_15y" },
         { data : "stockdata.0.terminal_years_format" },
 
-        { data : "stockdata.0.terminal_growth_rate_format" },
-        { data : "stockdata.0.discount_rate_format" },
+        { data : "stockdata.0.terminal_growth_rate_string_format" },
+        { data : "stockdata.0.discount_rate_string_format" },
 
         { data : "dcf_values_5y.growth_value" },
         { data : "dcf_values_5y.terminal_value" },
