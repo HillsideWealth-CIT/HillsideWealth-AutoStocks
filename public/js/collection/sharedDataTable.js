e098efc7078c070c67615437b9285b228c42a138
Siming Yang <yangsiming1999@gmail.com>
2019-08-07 14:00:57 -0700

    Added comments to code

--- public/js/collection/sharedDataTable.js
+++ public/js/collection/sharedDataTable.js
@@ -13,7 +13,7 @@ function Initialize_table(){
     ajax_Call("init_shared", "/init_table").then((resolve) => {
         stockdb = resolve.data;
         $table = fill_table(resolve.data);
-        console.log(stockdb)
+        console.log(stockdb);
         total_columns = $table.columns().header().length;
         $table.scroller.toPosition(stockdb.length,false);
         $table.scroller.toPosition(0);
